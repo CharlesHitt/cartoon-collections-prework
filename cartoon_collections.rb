0807babf23aa9f7c4c2951bc59fda6b316614a7f
Charles Hitt <charles.a.hitt@gmail.com>
2019-03-28 20:18:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,8 @@
 def roll_call_dwarves(array)# code an argument here
   # Your code here
   array.each_with_index do |name, index|
-    puts "#{index}. #{name}"
+    puts "#{index+1}. #{name}"
+end
 end
 
 def summon_captain_planet# code an argument here
